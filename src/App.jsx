f81366c8cf3f2ddf942805f40af0d57e599965e8
ViniMarkos283 <102962949+ViniMarkos283@users.noreply.github.com>
2025-07-15 12:13:32 -0300

    cliente sidebar/router Update
    - create sidebar for cliente
    - add FontAwesome
    - charge index CSS (global)
    - add icons in navbar
    - create sidebar from Adm
    - add all routes and subroutes

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,59 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// rotas primordiais 
 import HomePage from "./pages/homePage/homePage";
 import Login from "./pages/login/login";
 import Cadastro from "./pages/cadastro/cadastro";
 import Administrativo from "./pages/administrativo/administrativo";
 import Cliente from "./pages/cliente/cliente"
 
+// rotas do cliente 
+import AcomProgresso from "./components/Cliente/acompanharProgresso";
+import AgendaAula from "./components/Cliente/agendarAula";
+import ConsHorario from "./components/Cliente/consultaHorario";
+import Pagamento from "./components/Cliente/pagamento";
+import TreinoPerso from "./components/Cliente/treinoPersonalizado";
+
+// rotas administrativo
+import CadCliente from "./components/Administrativo/cadastrarCliente"
+import CadTreino from "./components/Administrativo/cadastrarTreinos"
+import ControlePresenca from "./components/Administrativo/controlePresenca"
+import GestaoPlano from "./components/Administrativo/gestaoPlanos"
+
+
 function App() {
 
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/administrativo" element={<Administrativo />} />
-        <Route path="/cliente" element={<Cliente />} />
-      </Routes>
-    </Router>
-      
+      <Router>
+        <Routes>
+          {/* rotas primordiais */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+
+          {/* rotas a partir do cliente */}
+          <Route path="/cliente" element={<Cliente />}>
+            <Route path="progresso" element={<AcomProgresso />} />
+            <Route path="agenda" element={<AgendaAula />} />
+            <Route path="horario" element={<ConsHorario />} />
+            <Route path="pagamento" element={<Pagamento />} />
+            <Route path="treinos" element={<TreinoPerso />} />
+          </Route>
+
+          {/* rotas a partir do administrativo */}
+          <Route path="/administrativo" element={<Administrativo />}>
+            <Route path="cadastrar-cliente" element={<CadCliente />} />
+            <Route path="cadastrar-treino" element={<CadTreino />} />
+            <Route path="controle-presenca" element={<ControlePresenca />} />
+            <Route path="gestao" element={<GestaoPlano />} />
+          </Route>
+
+
+        </Routes>
+      </Router>
+
     </>
   )
 }
--- src/pages/Administrativo/administrativo.jsx
+++ src/pages/Administrativo/administrativo.jsx
@@ -1,8 +1,28 @@
+// src/pages/administrativo/Administrativo.jsx
+
+import { Outlet, Link } from "react-router-dom";
+import style from "./administrativo.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket, faTableList, faUsersLine, faDumbbell, faToolbox } from '@fortawesome/free-solid-svg-icons';
+
 const Administrativo = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return (
+    <section className={style.view}>
+      <aside className={style.sidebar}>
+        <ul>
+        <li>Adminisitrativo</li>
+          <li><Link to="cadastrar-cliente"><FontAwesomeIcon icon={faUsersLine} />Cadastrar Cliente</Link></li>
+          <li><Link to="cadastrar-treino"><FontAwesomeIcon icon={faDumbbell} />Cadastrar Treino</Link></li>
+          <li><Link to="controle-presenca"><FontAwesomeIcon icon={faTableList} />Controle de Presença</Link></li>
+          <li><Link to="gestao"><FontAwesomeIcon icon={faToolbox} />Gestão de Planos</Link></li>
+          <li><Link to="/"><FontAwesomeIcon icon={faRightFromBracket} />Logout</Link></li>
+        </ul>
+      </aside>
+      <main className={style.content}>
+        <Outlet />
+      </main>
+    </section>
+  );
+};
 
 export default Administrativo;
